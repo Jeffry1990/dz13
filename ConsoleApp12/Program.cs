537b679dc05d9a96efd6c698661ae67bf61d4a28
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-13 09:24:47 +0300

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите любое слово: ");
+            string userMessage = Console.ReadLine();
+            Console.Write("Введите кол-во повторов: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < userNumber; i++)
+            {
+                Console.WriteLine("userMessage");
+            }
+        }
+    }
+}
